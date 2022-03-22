f6b88a1e708950d4eda6c724603cbad0d4d30237
Joakim Fröjvall Bejram <joakimfrojram@gmail.com>
2022-03-22 17:58:53 +0100

    skapat en funktion som framställer studenterna i databasen

--- courses.js
+++
@@ -1,5 +0,0 @@
-function onKeyUp() {
-    console.log(this.value);
-}
-let input = document.querySelector("input");
-input.addEventListener("keyup", onKeyUp);
--- students.js
+++ students.js
@@ -1,6 +1,23 @@
+//key up funktion
 function onKeyUp() {
     console.log(this.value);
 }
 let input = document.querySelector("input");
 input.addEventListener("keyup", onKeyUp);
 
+//Rendera och lägger in HTML element 
+
+function renderStudents(data) {
+
+    for (let i = 0; i < data.students.length; i++) {
+        let elementStudent = document.createElement("div")
+        elementStudent.innerHTML = `
+            <div id="container">
+                <h2>${data.students[i].firstName} ${data.students[i].lastName} (total: ${data.students[i].courses.passedCredits} )</h2>
+                <p> Courses </p>
+            </div>
+             `;
+        document.querySelector("#container").appendChild(elementStudent);
+    }
+}
+renderStudents(DATABASE);
